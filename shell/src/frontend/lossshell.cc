b1320dadb3c959172e3996d3e7fd4af9683331d0
andrewb1243 <115129576+andrewb1243@users.noreply.github.com>
2022-11-04 12:26:55 -0700

    Fixing lossshell.cc

--- shell/src/frontend/lossshell.cc
+++ shell/src/frontend/lossshell.cc
@@ -21,7 +21,6 @@ void usage( const string & program_name )
 int main( int argc, char *argv[] )
 {
     try {
-        const bool passthrough_until_signal = getenv( "MAHIMAHI_PASSTHROUGH_UNTIL_SIGNAL" );
 
         /* clear environment while running as root */
         char ** const user_environment = environ;
@@ -62,7 +61,7 @@ int main( int argc, char *argv[] )
             }
         }
 
-        PacketShell<IIDLoss> loss_app( "loss", user_environment, passthrough_until_signal );
+        PacketShell<IIDLoss> loss_app( "loss", user_environment );
 
         string shell_prefix = "[loss ";
         if ( link == "uplink" ) {
